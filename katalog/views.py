901496c128a096b66bcd9485734d86821c9fdc6e
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2022-09-13 23:27:17 +0700

    Assigment 2 DONE

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_item_catalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_item_catalog,
+    'name': 'Nafis Azizi Riza',
+    'npm' : '2106658843'
+}
+    return render(request, "katalog.html", context)
