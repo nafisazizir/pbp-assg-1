67c2c28d78b0db612a2e02c25013f4e343271307
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2022-09-22 01:29:57 +0700

    add testing and bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,19 +5,45 @@ from django.core import serializers
 
 def show_watchlist(request):
     data_watchlist_item = MyWatchListItem.objects.all()
+    not_yet = 0
+    watched = 0
+    mess = ""
+    for movies in data_watchlist_item:
+        if movies.watched:
+            watched +=1
+        else:
+            not_yet += 1
+    if watched >= not_yet:
+        mess = "Congratulations, you have watched a lot of movies!"
+    else:
+        mess  = "Woah, you need to watch more movies!"
     context = {
         'list_item': data_watchlist_item,
         'name' : 'Nafis Azizi Riza',
-        'npm' : 2106658843
+        'npm' : 2106658843,
+        'mess' : mess
     }
     return render(request, "mywatchlist.html", context)
 
 def show_html(request):
     data_watchlist_item = MyWatchListItem.objects.all()
+    not_yet = 0
+    watched = 0
+    mess = ""
+    for movies in data_watchlist_item:
+        if movies.watched:
+            watched +=1
+        else:
+            not_yet += 1
+    if watched >= not_yet:
+        mess = "Congratulations, you have watched a lot of movies!"
+    else:
+        mess  = "Woah, you need to watch more movies!"
     context = {
         'list_item': data_watchlist_item,
         'name' : 'Nafis Azizi Riza',
-        'npm' : 2106658843
+        'npm' : 2106658843,
+        'mess' : mess
     }
     return render(request, "mywatchlist.html", context)
 
